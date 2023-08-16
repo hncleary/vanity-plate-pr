4c17daa7365565edd3e8be79b65e7707b4bee876
Fernando <fernando@fernandos-mbp.attlocal.net>
2023-08-15 20:42:42 -0500

    started tiktok stat getter

---
+++ stats_getters/tiktok.ts
@@ -0,0 +1,25 @@
+import { BrowserContext, Page } from "playwright";
+import { convertAbbreviateNumberStr } from "../helper_functions/abbrev_num_convert";
+import { getBase64ImageFromUrl } from "../helper_functions/base64_url_img_fetch";
+
+
+// we want to follow the same concepts inside of the tiktok stats as we do with the instagram stats
+export class TiktokStats {
+    timeRetrieved: number = 0;
+    link: string = "";
+    displayName: string = "";
+    handle: string = "";
+    totalPosts: number = 0;
+    likes: number = 0;
+    followerCount: number = 0;
+    followingCount: number = 0;
+    iconUrl: string = "";
+    iconBase64: string = "";
+
+    public print() {
+        console.log('TikTok ' + this.displayName + ' Info:');
+        console.log('Handle (@): ' + this.handle);
+        console.log('Total Followers: ' + this.followerCount);
+        console.log('Total Following: ' + this.followingCount);
+    }
+}
