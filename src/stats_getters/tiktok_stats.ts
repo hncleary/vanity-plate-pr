ec408c96cafb7448ea596a4b18ff954f79f9333d
Hunter Cleary <34077789+hncleary@users.noreply.github.com>
2023-10-13 11:27:08 -0500

    Initialization of TikTok stats getter

--- src/stats_getters/tiktok_stats.ts
+++ src/stats_getters/tiktok_stats.ts
@@ -2,7 +2,7 @@ import { BrowserContext, Page } from 'playwright';
 import { convertAbbreviateNumberStr } from '../helper_functions/abbrev_num_convert';
 import { getBase64ImageFromUrl } from '../helper_functions/base64_url_img_fetch';
 
-export class TiktokStats {
+export class TikTokStats {
     timeRetrieved: number = 0;
     link: string = '';
     displayName: string = '';
@@ -22,4 +22,30 @@ export class TiktokStats {
     }
 }
 
-// TODO - follow stats retrieval pattern from instagram_stats.ts
+export async function getTikTokStatsArr(context: BrowserContext, usernames: string[]): Promise<TikTokStats[]> {
+    const stats: TikTokStats[] = [];
+    for (const username of usernames) {
+        const data = await getTikTokStats(context, username);
+        stats.push(data);
+    }
+    return stats;
+}
+
+export async function getTikTokStats(context: BrowserContext, handle: string): Promise<TikTokStats> {
+    const content = await getPageContent(context, handle);
+    const stats: TikTokStats = new TikTokStats();
+
+    // TODO - pull stats data from page content
+
+    return stats;
+}
+
+async function getPageContent(context: BrowserContext, username: string): Promise<string> {
+    const page: Page = await context.newPage();
+    const url = `https://www.tiktok.com/${username}?lang=en`;
+    await page.goto(url);
+    await page.waitForTimeout(5000);
+    const content = await page.content();
+    await page.close();
+    return content;
+}
