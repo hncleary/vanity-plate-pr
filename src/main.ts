8267d42c497514d7273751298b20d0974e3790be
Hunter Cleary <34077789+hncleary@users.noreply.github.com>
2023-12-01 19:06:56 -0600

    Fix Arg Count for Main

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { profileStatsGetter } from './profile_def_stats_getter';
 
 const args = process.argv.slice(2);
 
-if (args.length === 2) {
+if (args.length >= 2) {
     profileStatsGetter(args[0], args[1]);
 } else {
     profileStatsGetter();
