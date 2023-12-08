43dc46d5e79b0b12efaaea98231e6fa20ffb21ad
Hunter Cleary <34077789+hncleary@users.noreply.github.com>
2023-12-08 09:04:02 -0600

    Added colored logging via chalk

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import chalk = require('chalk');
 import { systemHasDisplay } from './helper_functions/has_display';
 import { profileStatsGetter } from './profile_def_stats_getter';
 
 const args = process.argv.slice(2);
 
 systemHasDisplay();
+
+console.log(chalk.green('Main Started'));
 
 if (args.length >= 2) {
     profileStatsGetter(args[0], args[1], args[2]);
